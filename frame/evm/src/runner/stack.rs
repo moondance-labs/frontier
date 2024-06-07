1dc2e065040abe5cd78cd4891a8da367a10e0785
Agustín Rodriguez <agus@moonsonglabs.com>
2024-06-07 15:05:13 -0300

    Customize origin for contract deployment (#1)
    * add support for contract deploy filter
    
    * add tests to pallet-ethereum
    
    * point to moondance evm fork
    
    * improve comments
    
    * fix comment in mock
    
    * add error comment
    
    * update evm branch

--- frame/evm/src/runner/stack.rs
+++ frame/evm/src/runner/stack.rs
@@ -50,8 +50,9 @@ use fp_evm::{
 use super::meter::StorageMeter;
 use crate::{
 	runner::Runner as RunnerT, AccountCodes, AccountCodesMetadata, AccountProvider,
-	AccountStorages, AddressMapping, BalanceOf, BlockHashMapping, Config, Error, Event,
-	FeeCalculator, OnChargeEVMTransaction, OnCreate, Pallet, RunnerError,
+	AccountStorages, AddressMapping, BalanceOf, BlockHashMapping, Config,
+    Error, EnsureCreateOrigin, Event, FeeCalculator,
+    OnChargeEVMTransaction, OnCreate, Pallet, RunnerError,
 };
 
 #[cfg(feature = "forbid-evm-reentrancy")]
@@ -515,6 +516,12 @@ where
 		proof_size_base_cost: Option<u64>,
 		config: &evm::Config,
 	) -> Result<CreateInfo, RunnerError<Self::Error>> {
+		let (_, weight) = T::FeeCalculator::min_gas_price();
+
+ 		T::CreateOrigin::check_create_origin(&source).map_err(|error| {
+			RunnerError {error, weight}
+		})?;
+
 		if validate {
 			Self::validate(
 				source,
@@ -570,6 +577,12 @@ where
 		proof_size_base_cost: Option<u64>,
 		config: &evm::Config,
 	) -> Result<CreateInfo, RunnerError<Self::Error>> {
+		let (_, weight) = T::FeeCalculator::min_gas_price();
+
+		T::CreateOrigin::check_create_origin(&source).map_err(|error| {
+		   RunnerError {error, weight}
+	   	})?;
+
 		if validate {
 			Self::validate(
 				source,
@@ -941,14 +954,14 @@ where
 		self.substate.set_deleted(address)
 	}
 
-	fn set_code(&mut self, address: H160, code: Vec<u8>) {
+	fn set_code(&mut self, address: H160, code: Vec<u8>, caller: Option<H160>) -> Result<(), ExitError>{
 		log::debug!(
 			target: "evm",
 			"Inserting code ({} bytes) at {:?}",
 			code.len(),
 			address
 		);
-		Pallet::<T>::create_account(address, code);
+		return Pallet::<T>::create_account(address, code, caller);
 	}
 
 	fn transfer(&mut self, transfer: Transfer) -> Result<(), ExitError> {
